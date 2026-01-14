b2f3ca92aa7c32f453b5f9755b2a1e5a8d6e76da
Ivan Barnash <aluhell122@gmail.com>
2026-01-14 16:44:56 -0500

    added profile-service and identity-service (authentication)

---
+++ api-gateway/build.gradle.kts
@@ -0,0 +1,42 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.5.8"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
+description = "api-gateway"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+extra["springCloudVersion"] = "2025.0.1"
+extra["tanzuScgExtensionsVersion"] = "1.0.1"
+
+dependencies {
+    implementation("com.vmware.tanzu.springcloudgateway.extensions:access-control")
+    implementation("org.springframework.cloud:spring-cloud-starter-gateway-server-webflux")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("com.vmware.tanzu.springcloudgateway.extensions:extensions-bom:${property("tanzuScgExtensionsVersion")}")
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
