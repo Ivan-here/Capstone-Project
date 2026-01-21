5f5aecc99af90a68ca9d8b8a6077414fccade51f
Mansi <mansichhillar8898@gmail.com>
2026-01-21 01:02:26 -0500

    added the updated logic for orders and resevations

---
+++ order_reservation-service/src/main/java/com/locally/orders/order_reservationservice/model/Reservation.java
@@ -0,0 +1,26 @@
+package com.locally.orders.order_reservationservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Document(collection = "reservations")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Reservation {
+    @Id
+    private String id;
+
+    private String ngoId;           // The NGO claiming the food
+    private String restaurantId;    // The Restaurant giving the food
+    private String surplusItemId;   // The specific food item ID (from Listing Service)
+
+    private String status = "RESERVED"; // Default status: RESERVED, PICKED_UP, CANCELLED
+
+    private LocalDateTime reservationTime = LocalDateTime.now(); // Auto-timestamp when created
+}
