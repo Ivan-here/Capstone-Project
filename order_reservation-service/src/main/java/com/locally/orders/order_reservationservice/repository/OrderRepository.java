39efced3f57d869f8360e8013bae2218012ce6a8
Mansi <mansichhillar8898@gmail.com>
2026-01-21 21:20:43 -0500

    changed the return types, added auth etc

--- order_reservation-service/src/main/java/com/locally/orders/order_reservationservice/repository/OrderRepository.java
+++ order_reservation-service/src/main/java/com/locally/orders/order_reservationservice/repository/OrderRepository.java
@@ -1,6 +1,7 @@
 package com.locally.orders.order_reservationservice.repository;
 
 import com.locally.orders.order_reservationservice.model.Order;
+import com.locally.orders.order_reservationservice.model.OrderStatus;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.List;
@@ -8,7 +9,7 @@ import java.util.List;
 public interface OrderRepository extends MongoRepository<Order, String>{
 
     List<Order> findByShopperId(String shopperId);
-    List<Order> findByStatus(String status);
+    List<Order> findByStatus(OrderStatus status);
     List<Order> findByRestaurantId(String restaurantId);
 
 
