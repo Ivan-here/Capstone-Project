13065f868e5d133b9856f6804468e5f8a8369740
artembaskoca@Gmail.com <artembaskoca@gmail.com>
2026-01-24 05:37:28 -0500

    notification services updated

--- notifications-service/src/main/java/com/example/notificationsservice/controller/PingController.java
+++ notifications-service/src/main/java/com/example/notificationsservice/controller/PingController.java
@@ -1,4 +1,4 @@
-package com.example.notificationsservice;
+package com.example.notificationsservice.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
