76bd7f9c494cd10c44e7d3ed14b215aacc78ebf9
SaKsHaTGaRg <sakshat2422@gmail.com>
2026-02-17 13:10:46 -0500

    fix

--- listing-service/src/main/java/com/example/listingservice/controller/ListingController.java
+++ listing-service/src/main/java/com/example/listingservice/controller/ListingController.java
@@ -14,6 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/listings")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:5173")
 public class ListingController {
 
     private final ListingService service;
