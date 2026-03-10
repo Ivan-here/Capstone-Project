f16f49e670f380cff9170919e554f9aece5981c9
Ivan Barnash <aluhell122@gmail.com>
2026-03-09 20:47:17 -0400

    admin panel WIP

---
+++ admin-service/src/main/java/com/example/clients/OrderReservationServiceClient.java
@@ -0,0 +1,5 @@
+package com.example.clients;
+
+public interface OrderReservationServiceClient {
+
+}
---
+++ admin-service/src/main/java/com/example/clients/VerificationServiceClient.java
@@ -0,0 +1,4 @@
+package com.example.clients;
+
+public interface VerificationServiceClient {
+}
