0b8cd82821a62778eba7c73ddb71d41cc4e9b26c
SaKsHaTGaRg <sakshat2422@gmail.com>
2026-03-19 23:13:32 -0400

    added reviews

--- review-service/build.gradle.kts
+++ review-service/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 group = "com.example"
 version = "0.0.1-SNAPSHOT"
-description = "CapstoneProject"
+description = "review-service"
 
 java {
     toolchain {
@@ -24,10 +24,20 @@ repositories {
     mavenCentral()
 }
 
+// REQUIRED: This allows Spring Cloud OpenFeign to work
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:2024.0.0")
+    }
+}
+
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
+
+    // NEW: Added for the Review Service logic
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+    implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
 
     compileOnly("org.projectlombok:lombok")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
