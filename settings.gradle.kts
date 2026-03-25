f2324794a388499ee4a485d834207b4a935b0cbd
SaKsHaTGaRg <sakshat2422@gmail.com>
2026-03-24 20:03:58 -0400

    fixed and tested ngo half time and reviews

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,3 @@
 rootProject.name = "CapstoneProject"
-include("listing-service","verification-service","identity-service","api-gateway","profile-service","order_reservation-service","notifications-service","community-service" , "review-service")
+include("listing-service","verification-service","identity-service","api-gateway","profile-service","order_reservation-service","notifications-service","community-service" , "review-service" , "stripe_payment-service")
 include("admin-service")
