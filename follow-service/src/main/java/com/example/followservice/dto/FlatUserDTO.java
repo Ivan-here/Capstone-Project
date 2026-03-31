98b7f727e35ea7245ffbe2e8dc4f758e66e0b8b8
SaKsHaTGaRg <sakshat2422@gmail.com>
2026-03-31 18:34:09 -0400

    added follow logic

---
+++ follow-service/src/main/java/com/example/followservice/dto/FlatUserDTO.java
@@ -0,0 +1,15 @@
+package com.example.followservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class FlatUserDTO {
+    private String id;
+    private String displayName;
+    private String role;
+    private String avatarUrl;
+}
