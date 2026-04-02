c44caa92e4b7511724515e71e7e335c679297b01
artembaskoca@Gmail.com <artembaskoca@gmail.com>
2026-04-01 21:56:09 -0400

    adding notifications to orders, follows, fix ui for follows

---
+++ listing-service/src/main/java/com/example/listingservice/dto/internal/FlatUserDTO.java
@@ -0,0 +1,11 @@
+package com.example.listingservice.dto.internal;
+
+import lombok.Data;
+
+@Data
+public class FlatUserDTO {
+    private String id;
+    private String displayName;
+    private String role;
+    private String avatarUrl;
+}
